e7ccd4203332cd6b5f0ab313b2eae4503764ec06
Emanuel Santos Souza de Jesus <emanuelsantoss42@gmail.com>
2022-12-07 22:22:27 -0300

    API || Finalizada

---
+++ WorldCup/WorldCupAPI/Models/WorldCupCountryPlayer.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorldCupAPI.Models;
+
+[Table("WorldCupCountryPlayer")]
+public class WorldCupCountryPlayer
+{
+    [Key, Column(Order = 1)]
+    public int WorldCupId { get; set; }
+
+    [ForeignKey("WorldCupId")]
+    public WorldCup WorldCup { get; set; }
+
+    [Key, Column(Order = 2)]
+    public int CountryId { get; set; }
+
+    [ForeignKey("CountryId")]
+    public Country Country { get; set; }
+
+    [Key, Column(Order = 3)]
+    public int PlayerId { get; set; }
+    [ForeignKey("playerId")]
+    public Player Player { get; set; }
+}
+
+
